0364884d1f4539464eb7028e778a60b367376fc4
Bahaa Abou Yassine <bahaa.abouyassine2@gmail.com>
2023-08-28 16:39:04 +0300

    ➕ Creating a MongoDB connection

--- models/product.js
+++ models/product.js
@@ -1,7 +1,23 @@
-const Sequelize = require('sequelize');
 
-const sequelize = require("../util/database");
+const getDb = require('../util/database').getDb;
+class Product {
+  constructor(id, title, description, price, imageUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.imageUrl = imageUrl;
+  }
+  save() {
+    const db = getDb();
+    db.collection('products').insertOne(this).then(result => {
+      console.log(result);
+    }).catch(err => { 
+      console.log(err);
+     });
 
+  }
+}
 
 const Product = sequelize.define('product', {
   id: {
